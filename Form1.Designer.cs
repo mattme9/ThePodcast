07c7d2547a18081664e93e6ae5380b1e8658796f
dkarlsson1996 <dkarlsson1996@gmail.com>
2023-10-18 16:09:28 +0200

    gui och bugupdate

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtUrl = new TextBox();
+            lblUrl = new Label();
+            lblCategory = new Label();
+            boxCategory = new ComboBox();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            btnChange = new Button();
+            listPoddar = new ListBox();
+            textBox1 = new TextBox();
+            lblName = new Label();
+            SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            txtUrl.Location = new Point(12, 70);
+            txtUrl.Name = "txtUrl";
+            txtUrl.Size = new Size(295, 27);
+            txtUrl.TabIndex = 0;
+            // 
+            // lblUrl
+            // 
+            lblUrl.AutoSize = true;
+            lblUrl.Location = new Point(12, 47);
+            lblUrl.Name = "lblUrl";
+            lblUrl.Size = new Size(38, 20);
+            lblUrl.TabIndex = 1;
+            lblUrl.Text = "URL:";
+            lblUrl.Click += label1_Click;
+            // 
+            // lblCategory
+            // 
+            lblCategory.AutoSize = true;
+            lblCategory.Location = new Point(12, 106);
+            lblCategory.Name = "lblCategory";
+            lblCategory.Size = new Size(72, 20);
+            lblCategory.TabIndex = 2;
+            lblCategory.Text = "Category:";
+            lblCategory.Click += label1_Click_1;
+            // 
+            // boxCategory
+            // 
+            boxCategory.FormattingEnabled = true;
+            boxCategory.Location = new Point(12, 129);
+            boxCategory.Name = "boxCategory";
+            boxCategory.Size = new Size(95, 28);
+            boxCategory.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(13, 179);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 4;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(113, 179);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnChange
+            // 
+            btnChange.Location = new Point(213, 179);
+            btnChange.Name = "btnChange";
+            btnChange.Size = new Size(94, 29);
+            btnChange.TabIndex = 6;
+            btnChange.Text = "Change";
+            btnChange.UseVisualStyleBackColor = true;
+            // 
+            // listPoddar
+            // 
+            listPoddar.FormattingEnabled = true;
+            listPoddar.ItemHeight = 20;
+            listPoddar.Location = new Point(12, 229);
+            listPoddar.Name = "listPoddar";
+            listPoddar.Size = new Size(295, 284);
+            listPoddar.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(138, 130);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(169, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(138, 107);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(52, 20);
+            lblName.TabIndex = 9;
+            lblName.Text = "Name:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1017, 530);
+            Controls.Add(lblName);
+            Controls.Add(textBox1);
+            Controls.Add(listPoddar);
+            Controls.Add(btnChange);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(boxCategory);
+            Controls.Add(lblCategory);
+            Controls.Add(lblUrl);
+            Controls.Add(txtUrl);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUrl;
+        private Label lblUrl;
+        private Label lblCategory;
+        private ComboBox boxCategory;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnChange;
+        private ListBox listPoddar;
+        private TextBox textBox1;
+        private Label lblName;
     }
 }
