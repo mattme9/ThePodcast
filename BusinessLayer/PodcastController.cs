c0221ca73cc8fe71587d5f85497692a85c50d188
mattiasmesserer <mattias.messerer@gmail.com>
2023-10-18 16:49:11 +0200

    la till klasser (några saknas)

---
+++ BusinessLayer/PodcastController.cs
@@ -0,0 +1,17 @@
+﻿namespace BusinessLayer
+{
+
+    //Denna klass används till att komma åt DataAccessLayer
+    public class PodcastController
+    {
+        //private IPodcastRepository podcastRepository;
+        public PodcastController() 
+        { 
+            //podcastRepository = new PodcastRepository();
+        }
+
+        //public void createPod()
+
+        //public List<Podcast> GetAllPodcasts
+    }
+}
---
+++ DataAccessLayer/DataManager.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccessLayer
+{
+
+    //Denna klass ska hantera användarens datafil (XML Serializer etc).
+    public class DataManager
+    {
+        
+    }
+
+    //public void Serialize
+
+    //public void Deserialize
+}
